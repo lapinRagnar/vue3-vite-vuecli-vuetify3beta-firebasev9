d5070f961528a9547821e330ccfce86f01e8bde5
lapinRagnar <lapinragnar@gmail.com>
2022-11-06 18:38:49 +0100

    bouton small - large - x-large

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,17 +3,39 @@
     <h1> home</h1>
     
     <div class="mb-5">
-      <v-btn width="50%" depressed  dark color="pink">click moi</v-btn>
+      <v-btn depressed  dark color="pink">click moi</v-btn>
     </div>
 
-    <div >
-      <v-btn width="50%" flat color="bg-red" >click moi</v-btn>
+    <div class="mb-5">
+      <v-btn  flat color="bg-red" >click moi</v-btn>
     </div>
 
-    <v-btn width="50%" class="bg-red-accent-4 text-green">
-      <v-icon left>mdi-email</v-icon>
-      <span>email moi</span>
-    </v-btn>
+    <div class="mb-5">
+      <v-btn class="bg-red-accent-4 text-green">
+        <v-icon left>mdi-email</v-icon>
+        <span>email moi</span>
+      </v-btn>
+    </div>
+
+    <div class="mb-5">
+      <v-btn small class="bg-red-accent-4 text-green">
+        <v-icon small left>mdi-email</v-icon>
+        <span>email moi</span>
+      </v-btn>
+    </div>
+
+    <div class="mb-5">
+      <v-btn x-large class="bg-yellow-lighten-2 text-green">
+        <span>email moi</span>
+        <v-icon x-large right>mdi-email</v-icon>
+      </v-btn>
+    </div>
+    <div class="mb-5">
+      <v-btn size="small"  class="bg-yellow-lighten-2 text-green">
+        <span>email moi</span>
+        <v-icon x-large right>mdi-email</v-icon>
+      </v-btn>
+    </div>
   
   </div>
 </template>
