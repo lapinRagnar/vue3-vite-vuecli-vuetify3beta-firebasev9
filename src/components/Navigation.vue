374ff4fa8e36d7cb363821521c194beddcc3d554
lapinRagnar <lapinragnar@gmail.com>
2022-11-06 17:30:12 +0100

    navigation retouche

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,44 +6,27 @@
       dense
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
       <v-toolbar-title>{{ titre }} - Tuto Vuetify</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+        <v-icon>mdi-home</v-icon>
       </v-btn>
 
       <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
+        <v-icon>mdi-information-variant</v-icon>
       </v-btn>
 
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
+      <v-btn icon>
+        <v-icon>mdi-account-lock</v-icon>
+      </v-btn>
 
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <v-btn icon>
+        <v-icon>mdi-account-off</v-icon>
+      </v-btn>
+
     </v-app-bar>
 
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     Home
+    
+    <p class="purple red--text text-center">Lorem ipsum</p>
+
+    <div class="purple darken-2 text-center">
+      <span class="white--text">Lorem ipsum</span>
+    </div>
+    
+
+    <v-btn outlined flat color="primary">
+      <v-icon dense color="error">mdi-email</v-icon>
+      <span class="white--text"> Click </span>
+    </v-btn>
   </div>
 </template>
 
