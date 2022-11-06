ad89fbb65cf6ae5f5f480d970f779644c2ac6077
lapinRagnar <lapinragnar@gmail.com>
2022-11-06 22:53:01 +0100

    6 - layout - drawer - ajouter route / et about

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,57 @@
     
     <Navigation />
 
-    <v-main>
-      <router-view/>
-    </v-main>
+    <!-- layout -->
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer
+          v-model="drawer"
+          :rail="rail"
+          width="180"
+          permanent
+          @click="rail = false"
+        >
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+            title="John Leider"
+            nav
+          >
+            <template v-slot:append>
+              <v-btn
+                variant="text"
+                icon="mdi-chevron-left"
+                @click.stop="rail = !rail"
+              ></v-btn>
+            </template>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list density="compact" nav>
+            <v-list-item :to="{name: 'home'}" prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+            <v-list-item :to="{name: 'about'}" prepend-icon="mdi-information-variant" title="About" value="about"></v-list-item>
+            <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <v-main style="height: 300px;">
+          <router-view/>
+        </v-main>
+      </v-layout>
+    </v-card>
+
+
   </v-app>
 </template>
 
 <script setup>
 
+import { ref, onMounted, computed  } from 'vue'
+
 import Navigation from '@/components/Navigation.vue'
+
+const drawer = ref(true)
+const rail = ref(true)
+
 
 </script>
