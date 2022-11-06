415f34d0b8a082ac4d7fe1b340942a0eff6129f0
lapinRagnar <lapinragnar@gmail.com>
2022-11-06 19:25:26 +0100

    afficher quand c'est sm

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,9 @@
     <!-- <v-btn class="hidden-md-and-down" depressed  dark color="pink">click moi</v-btn> -->
     
     <!-- cache le bouton quand la taille de l'ecran est supérieur à 1264         -->
-    <v-btn class="hidden-md-and-up" depressed  dark color="pink">click moi</v-btn>
+    <!-- <v-btn class="hidden-md-and-up" depressed  dark color="pink">click moi</v-btn> -->
 
+    <v-btn class="d-sm-none d-md-flex">clique-moi</v-btn>
   
   </div>
 </template>
