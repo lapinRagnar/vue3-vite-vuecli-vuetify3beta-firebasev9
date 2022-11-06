1eb131e060358809d0c89c4437fa0c328565fb6e
lapinRagnar <lapinragnar@gmail.com>
2022-11-06 19:20:17 +0100

    cache le bouton quand l'ecran est superieur à 1264

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <h1> home</h1>
-    <!-- cache le bouton quand la taille de l'ecran est entre 960 et 1264         -->
-    <v-btn class="hidden-md-and-down" depressed  dark color="pink">click moi</v-btn>
+    <!-- cache le bouton quand la taille de l'ecran est inférieur à 1264         -->
+    <!-- <v-btn class="hidden-md-and-down" depressed  dark color="pink">click moi</v-btn> -->
+    
+    <!-- cache le bouton quand la taille de l'ecran est supérieur à 1264         -->
+    <v-btn class="hidden-md-and-up" depressed  dark color="pink">click moi</v-btn>
 
   
   </div>
