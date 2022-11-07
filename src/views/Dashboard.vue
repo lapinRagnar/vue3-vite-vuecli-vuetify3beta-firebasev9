b446d5c16cc8ede41e83ecc8150763e25f7c1003
lapinRagnar <lapinragnar@gmail.com>
2022-11-07 20:54:15 +0100

    suite grid de la dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,22 +3,34 @@
     <h1 class="text-h3 text-brown-darken-2 text-center"> Dashboard</h1>
 
     <v-container fluid  class="my-5">
+
+      <v-card class="pa-4">
       
-      <v-row >
+        <v-row>
+          
+          <v-col cols="12" md="6">
+            <div class="caption text-grey">Project Title</div>
+            <div>Create a new website</div>
+          </v-col>
 
-        <v-col cols="12" sm="12" md="6" lg="6">
-          <v-sheet class="pa-2 ma-2">
-            <v-btn small outline block color="primary">1</v-btn> 
-          </v-sheet>                     
-        </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption text-grey">Person</div>
+            <div>La bla da bla</div>
+          </v-col>
 
-        <v-col cols="12" sm="12" md="6" lg="6">
-          <v-sheet class="pa-2 ma-2">
-            <v-btn small outline block color="secondary">2</v-btn> 
-          </v-sheet>                     
-        </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption text-grey">Du by</div>
+            <div>07/11/2022</div>
+          </v-col>
 
-      </v-row>
+          <v-col cols="2" sm="4" md="2">
+            <div class="caption text-grey">Status</div>
+            <div>ongoing</div>
+          </v-col>
+
+        </v-row>
+      
+      </v-card>
 
     </v-container>
   
@@ -27,7 +39,5 @@
 
 <script setup>
 
-
-// Components
 
 </script>
