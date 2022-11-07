4d9195648c66704fe25e89bd7844fa0380d89847
lapinRagnar <lapinragnar@gmail.com>
2022-11-07 20:37:34 +0100

    11 - grid system - breakpoint cols="12" sm="12" md="6" lg="6"

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,8 +11,4 @@
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
 
-}
-</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <h1 class="text-h3 text-brown-darken-2 text-center"> home</h1>
+    <h1 class="text-h3 text-brown-darken-2 text-center"> Dashboard</h1>
 
-    <v-container grid-list-xs class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores expedita facilis commodi atque totam, debitis nobis! Doloremque, ducimus totam fugiat soluta omnis maxime cumque unde exercitationem error tempore animi sunt?</p>
-      <p class="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati atque nemo similique molestias porro neque dolorem quibusdam iure inventore ratione adipisci facere, mollitia repudiandae veniam illum. Possimus, illum quibusdam!</p>
+    <v-container fluid  class="my-5">
+      
+      <v-row >
+
+        <v-col cols="12" sm="12" md="6" lg="6">
+          <v-sheet class="pa-2 ma-2">
+            <v-btn small outline block color="primary">1</v-btn> 
+          </v-sheet>                     
+        </v-col>
+
+        <v-col cols="12" sm="12" md="6" lg="6">
+          <v-sheet class="pa-2 ma-2">
+            <v-btn small outline block color="secondary">2</v-btn> 
+          </v-sheet>                     
+        </v-col>
+
+      </v-row>
+
     </v-container>
   
   </div>
